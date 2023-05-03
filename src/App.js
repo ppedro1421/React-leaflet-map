0bd5fa160797feb9662785c4f73fdbeb72052421
ppedro1421 <93495026+ppedro1421@users.noreply.github.com>
2023-05-03 09:53:09 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet'
+//import { Marker, Popup, Circle, ImageOverlay } from 'react-leaflet'
+
+import './css/leaflet.css'
+
+//var L = require('leaflet');
+const center = [-22.41358, -42.98126]
+
+function Map() {
+  return (
+    <div id="map">
+      <MapContainer center={center} zoom={10} minZoom={2} scrollWheelZoom={true}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer name="openstreetmap" checked={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="mapbox satellite">
+            <TileLayer
+              attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+              url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
