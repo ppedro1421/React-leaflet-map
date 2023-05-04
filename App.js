13888cd4113e7932123e3396aa27fe369857ae8d
ppedro1421 <93495026+ppedro1421@users.noreply.github.com>
2023-05-04 18:05:54 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import { MapContainer, TileLayer, LayersControl, LayerGroup } from 'react-leaflet'
+// eslint-disable-next-line
+import { Marker, Popup, Circle, Rectangle } from 'react-leaflet'
+
+import './css/leaflet.css'
+
+const center = [-22.906138, -43.174528]
+
+// Bounds
+const worldBound = [[-180, 180], [180, -180]]
+const southAmericaBound = [[-60, 30], [20, -140]]
+
+function Map() {
+  return (
+    <div id="map">
+      <MapContainer
+        center={center}
+        zoom={8}
+        zoomSnap={0.5}
+        zoomDelta={0.5}
+        minZoom={3}
+        maxZoom={18}
+        scrollWheelZoom={true}
+        worldCopyJump={true}
+        maxBounds={worldBound}
+        maxBoundsViscosity={0.99}
+      >
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer name="openstreetmap" checked={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="mapbox satellite">
+            <TileLayer
+              attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+              url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.Overlay name='South America Bound'>
+            <Rectangle bounds={southAmericaBound}>
+              <Popup>South America Bound</Popup>
+            </Rectangle>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name='World Bound'>
+            <Rectangle bounds={worldBound}>
+              <Popup>World Bound</Popup>
+            </Rectangle>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name='Marcos'>
+            <LayerGroup>
+              <Circle center={[-22.906138, -43.174528]} radius={50000} pathOptions={{ color: 'red' }}>
+                <Popup>RJ</Popup>
+              </Circle>
+              <Circle center={[-23.535141, -46.623467]} radius={50000} pathOptions={{ color: 'red' }}>
+                <Popup>SP</Popup>
+              </Circle>
+            </LayerGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
